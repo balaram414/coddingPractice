165af961f842502f01293ae7915555acea88c435
Balaram <balarammanadal2404@gmail.com>
2023-12-21 22:59:58 +0530

    vowels count

--- countVowels.js
+++ countVowels.js
@@ -1,10 +1,14 @@
 // count vowel and consonants from String
-let arr={"vowel":0,"consonants":0};
-let str="I would like to inform that I am balaram 'Mandal'"
-let r=str[0].match(/^[^a-zA-Z9]+$/)
-
-// for(let i=0;i<str.length;i++)
-// {
-    
-// }
-
+let arr = { vowel: 0, consonants: 0 };
+let vowel = { a: true, e: true, i: true, o: true, u: true };
+let str = "I would like to inform that I am balaram 'Mandal'";
+let valCount=0;
+let conCount=0;
+for (let i = 0; i < str.length; i++) {
+  let val = str[i].toLocaleLowerCase();
+  let r = /^[a-zA-Z]+$/.test(str[i]);
+  if (val in vowel)valCount++;
+  else if(r)conCount++;
+}
+console.log("vowel: "+valCount)
+console.log("consonants: "+conCount)
