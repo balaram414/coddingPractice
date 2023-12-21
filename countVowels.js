9f23320755c01a8ada15084e0a8bb7e3808bbbc2
Balaram <balarammanadal2404@gmail.com>
2023-12-21 21:54:14 +0530

    Link list

---
+++ countVowels.js
@@ -0,0 +1,10 @@
+// count vowel and consonants from String
+let arr={"vowel":0,"consonants":0};
+let str="I would like to inform that I am balaram 'Mandal'"
+let r=str[0].match(/^[^a-zA-Z9]+$/)
+
+// for(let i=0;i<str.length;i++)
+// {
+    
+// }
+
